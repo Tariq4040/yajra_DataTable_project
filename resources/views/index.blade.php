c4814384c885c2f72720710c4c278ba7a92d511d
TariqAhmad <chtariq14409@gmail.com>
2022-06-13 16:53:28 +0500

    index file is added to the main view file

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Yajra Table</title>
+</head>
+<body>
+    <div>
+        <h1>Home Page</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique sapiente, ipsum ut, omnis praesentium magnam incidunt eligendi officiis maiores accusamus, inventore voluptatibus culpa iure asperiores blanditiis? Nobis consequuntur temporibus error eum perferendis quasi laborum voluptatem? Veniam, minima libero? Omnis, obcaecati sunt perspiciatis temporibus labore minima hic tenetur. Voluptatem ipsum itaque aspernatur, esse exercitationem et dolorum quod corporis. Deleniti dignissimos culpa, corrupti architecto velit voluptatibus, placeat, modi perferendis assumenda quisquam tenetur facere. Iste soluta illo magnam molestiae, nostrum qui magni recusandae ipsum explicabo ad? Reprehenderit numquam eos labore, molestias inventore doloremque recusandae quae, vero nulla quasi sit reiciendis! Deleniti, maxime temporibus.
+        </p>
+    </div>
+</body>
+</html>
