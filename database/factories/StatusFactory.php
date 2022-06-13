45429bd4df7978f9f6dac188daf2587b052ec117
TariqAhmad <chtariq14409@gmail.com>
2022-06-13 10:36:17 +0500

    yajrab datatable project uploaded

---
+++ database/factories/StatusFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class StatusFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $status = $this->faker->randomElement(['1' , '0']);
+        return [
+            'name'=>$this->faker->name,
+            'email'=>$this->faker->email,
+            'status'=>$status
+        ];
+    }
+}
